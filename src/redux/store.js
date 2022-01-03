202739bbc3f76117ad454268f875326d62e53753
kazufoot21 <kazufoot21@outlook.jp>
2022-01-03 14:38:24 +0900

    add slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
