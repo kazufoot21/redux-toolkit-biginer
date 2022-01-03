64b8af1547e04fe011e6d82a21b5fac16603a9e4
kazufoot21 <kazufoot21@outlook.jp>
2022-01-03 14:47:18 +0900

    set useSelector and useDispatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increase, decrease } from './redux/counterSlice';
 
 function App() {
-  const [count, setCount] = useState(0);
+  //counterSliceで設定したcountの値を取得
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>UP</button>
-      <button onClick={() => setCount(count - 1)}>DOWN</button>
+      <button onClick={() => dispatch(increase())}>UP</button>
+      <button onClick={() => dispatch(decrease())}>DOWN</button>
     </div>
   );
 }
